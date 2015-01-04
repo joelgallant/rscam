97e777fad51d587bc9d712e8011fd78fd897158a
Paul Loyd <pavelko95@gmail.com>
2015-01-04 04:48:41 +0300

    updated docs

---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rscam'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='rscam/struct.Frame.html' title='rscam::Frame'>Frame</a>&lt;'a&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='rscam/struct.Camera.html' title='rscam::Camera'>Camera</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
