da344c4bb0807f9fbba9029b4568c0f1437aba4b
Paul Loyd <pavelko95@gmail.com>
2015-01-30 20:24:08 +0300

    chore: update docs

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rscam'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='rscam/enum.Error.html' title='rscam::Error'>Error</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='rscam/struct.FormatInfo.html' title='rscam::FormatInfo'>FormatInfo</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='rscam/enum.ResolutionInfo.html' title='rscam::ResolutionInfo'>ResolutionInfo</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='rscam/enum.IntervalInfo.html' title='rscam::IntervalInfo'>IntervalInfo</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rscam'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='rscam/enum.Field.html' title='rscam::Field'>Field</a>","<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='rscam/struct.Config.html' title='rscam::Config'>Config</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
